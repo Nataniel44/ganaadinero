50b6dad060c977f5351747ac64faf4f1cca4ecb6
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-11-15 18:40:51 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         
         <Route path="/" element={<Home />} />
+        <Route path="/ganaadinero/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
